9f6046990805b1a89c03d4cc94de32a1de6354eb
chendt <1793472283@163.com>
2020-12-30 20:14:10 +0800

    dev:添加一个主函数

--- src/test/java/com/cn/UserTests.java
+++ src/test/java/com/cn/UserTests.java
@@ -1,4 +1,16 @@
 package com.cn;
 
+import org.junit.jupiter.api.Test;
+
 public class UserTests {
+    @Test
+    void test1() {
+        int c=3;
+        int e=5;
+    }
+
+    public static void main(String[] args) {
+        int a=5;
+        System.out.println(a);
+    }
 }
